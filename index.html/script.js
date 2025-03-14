a8e5f7b09a431b7bebc264699f7fa9eb7e7357c4
Nicz04 <bolfangon@gmail.com>
2025-03-14 17:27:44 +0800

    Add files via upload

---
+++ index.html/script.js
@@ -0,0 +1,13 @@
+    alert("Welcome")
+    prompt("Do you like JavaScript?")
+
+    if(userName)
+        prompt("Hello, Glad you like JavaScript") 
+        prompt("Hello, Hope you learn to enjoy it")
+    if(userName) prompt("What's your Name?")
+       
+    let message = likesJS
+    confirm("Do you like JavaScript?");
+    document.getElementById("output").textContent = message;
+  
+   
